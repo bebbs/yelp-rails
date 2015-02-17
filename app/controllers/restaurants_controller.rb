5642693074611c95be4e41ab5462790a4c8ac4ca
Josh Bebbington <josh@focalwebsites.com>
2015-02-17 12:30:22 +0000

    Added a single restaurant view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,11 +4,16 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
-    Restaurant.create(restaurant_params)
+    @restaurant = Restaurant.create(restaurant_params)
     redirect_to '/restaurants'
   end
 
