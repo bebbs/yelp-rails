1f3fcfbf48066b2e4780fc844edcf34361b0416c
Josh Bebbington <josh@focalwebsites.com>
2015-02-18 10:57:10 +0000

    Added omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   root to: "restaurants#index"
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  # devise_scope :user do
+  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
 
   resources :restaurants do
     resources :reviews
